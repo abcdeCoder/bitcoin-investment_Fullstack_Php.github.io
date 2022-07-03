1412a4ca02fdd9291e0814514707a2862b68089c
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-07-03 21:16:52 +0530

    update

--- core/config/database.php
+++ core/config/database.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Str;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -34,7 +33,6 @@ return [
     */
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
@@ -46,11 +44,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'remotemysql.com'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'Qhi5r1FFe0'),
-            'username' => env('DB_USERNAME', 'Qhi5r1FFe0'),
-            'password' => env('DB_PASSWORD', 'UaSlm473Kv'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -58,9 +56,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ])
+                : [],
         ],
 
         'pgsql' => [
@@ -90,7 +90,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
     ],
 
     /*
@@ -118,28 +117,26 @@ return [
     */
 
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix' =>
+                Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_',
         ],
 
         'default' => [
-            'host' => env('REDIS_HOST', 'remotemysql.com'),
-            'password' => env('REDIS_PASSWORD', UaSlm473Kv),
+            'host' => env('REDIS_HOST', ''),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', 3306),
             'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [
-            'host' => env('REDIS_HOST', 'remotemysql.com'),
-            'password' => env('REDIS_PASSWORD', UaSlm473Kv),
+            'host' => env('REDIS_HOST', ''),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', 3306),
             'database' => env('REDIS_CACHE_DB', 1),
         ],
-
     ],
-
 ];
